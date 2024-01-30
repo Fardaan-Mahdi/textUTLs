dfe234971aa5432b810f0f0bef907b90b1705bbc
Fardaan Mahdi <codingmonk501@gmail.com>
2024-01-31 02:09:10 +0530

    Adding More Logic

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,21 +1,25 @@
 import React, { useState } from "react";
 
 function TextForm(props) {
-  const [text, setText] = useState("Enter Text Here");
-  const handleClick=()=>{
+  const [text, setText] = useState("");
+  const handleUPClick=()=>{
     console.log("button was clicked:!");
     setText(text.toUpperCase());
+  }
+  const handleDOWNClick=()=>{
+    console.log("button was clicked:!");
+    setText(text.toLowerCase());
   }
   const handleChange=(e)=>{
     console.log("on change");
     setText(e.target.value)
   }
   function handleClear(){
-    setText("Enter Text Here")
+    setText("")
   }
   return (
     <>
-      <div className="mb-3">
+      <div className="container"><div className="mb-3">
         <label for="myBox" className="form-label">
           {props.heading}
         </label>
@@ -26,8 +30,17 @@ function TextForm(props) {
           value={text}
           onChange={handleChange}
         ></textarea>
-        <button className="btn btn-primary my-3" onClick={handleClick}>UpperCase</button>
+        <button className="btn btn-primary my-3" onClick={handleUPClick}>UpperCase</button>
+        <button className="btn btn-primary ms-3" onClick={handleDOWNClick}>LowerCase</button>
         <button className="btn btn-secondary mx-3" onClick={handleClear}>Clear</button>
+      </div></div>
+      <div className="container">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <p>Time to Read: {0.48* text.split(" ").length} sec</p>
+        <h1>Preview</h1>
+        <p>{text}</p>
+
       </div>
     </>
   );
